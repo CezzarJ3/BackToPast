9d08d91a843ced0704808892949bda5c19cca3f1
CezzarJ3 <silenia2014@gmail.com>
2022-09-10 13:42:02 +0300

    Initial commit

---
+++ src/main/java/cs/julia/backtopast/BackToPastApplication.java
@@ -0,0 +1,13 @@
+package cs.julia.backtopast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToPastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackToPastApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cs/julia/backtopast/BackToPastApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.julia.backtopast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackToPastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
